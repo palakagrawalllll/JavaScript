3e7e06b1878dbcc93a0399e8e0f620aacdac16c8
palakagrawalllll <palakagrawal020406@gmail.com>
2025-11-26 19:07:33 +0000

    Datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -10,3 +10,23 @@ console.log(typeof (pal));
 
 valueInNumber = Number(pal)
 console.log (typeof valueInNumber)
+
+let wall = Number("33qwe")
+console.log(wall);
+console.log(typeof (wall));
+
+valueInNumber = Number(wall)
+console.log (typeof valueInNumber)
+
+let isLoggedIn = 1
+let booleanisLogggedIn = Boolean(isLoggedIn)
+console.log(booleanisLogggedIn)
+
+let isSubscribed = 0
+let booleanisSubscribed = Boolean(isSubscribed)
+console.log(booleanisSubscribed)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
