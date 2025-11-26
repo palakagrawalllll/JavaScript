40c614681cc57fa09205e4829ec38345546872dc
palakagrawalllll <palakagrawal020406@gmail.com>
2025-11-26 18:19:45 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+let score = "3"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+let pal = 33
+console.log(typeof pal);
+console.log(typeof (pal));
+
+valueInNumber = Number(pal)
+console.log (typeof valueInNumber)
